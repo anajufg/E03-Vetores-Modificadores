19835cc5c84671f2e52ed41339f94ce28df43ba1
Ana Julia <anajufg127@gmail.com>
2024-04-12 21:27:36 -0300

    Update Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -1,23 +1,86 @@
-
 public class Conta {
 
-    Cliente dono = new Cliente();
-    double saldo;
-    int numero;
-    double limite;
-    String agencia;
+    private Cliente dono = new Cliente();
+    private double saldo = 0;
+    private int numero = -1;
+    private double limite = 0;
+    private Operacao[] operacoes = new Operacao[1000];;
+    private int numOperacoes = 0;
+    private static int totalContas;
 
-    Operacao[] operacoes = new Operacao[1000];
+    public Conta() {
+        Conta.totalContas++;
+    }
 
-    int numOperacoes = 0;
+    // Get totalContas
+    public static int getTotalContas() {
+        return Conta.totalContas;
+    }
 
-    boolean depositar(double valor) {
+    // Get numOperacoes
+    public int getNumOperacoes() {
+        return this.numOperacoes;
+    }
+
+
+    // Set dono
+    public void setDono(Cliente dono) {
+        this.dono = dono;
+    }
+
+
+    // Get dono
+    public Cliente getDono() {
+        return this.dono;
+    }
+
+
+    // Set numero
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+
+    // Get numero
+    public int getNumero() {
+        return this.numero;
+    }
+
+
+    // Get saldo
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+
+    // Set limite
+    public boolean setLimite(double limite) {
+        if (limite < 0) {
+            this.limite = 0;
+            System.out.println("Limite inválido.");
+            return false;
+        } else {
+            this.limite = limite;
+            return true;
+        }
+    }
+
+
+    // Get numero
+    public double getLimite() {
+        return this.limite;
+    }
+
+
+    public boolean depositar(double valor) {
         if (this.numOperacoes < 1000) {
 
-            this.operacoes[numOperacoes] = new Operacao('d', valor);
-            this.numOperacoes++;
 
             if (valor > 0.0) {
+                this.operacoes[numOperacoes] = new Operacao('d', valor);
+                this.numOperacoes++;
+
+
                 this.saldo += valor;
                 return true;
             } else {
@@ -30,13 +93,15 @@ public class Conta {
     }
 
 
-    boolean sacar(double valor) {
+    public boolean sacar(double valor) {
         if (this.numOperacoes < 1000) {
 
-            this.operacoes[numOperacoes] = new Operacao('s', valor);
-            this.numOperacoes++;
 
             if (valor > 0.0 && valor <= this.saldo) {
+                this.operacoes[numOperacoes] = new Operacao('s', valor);
+                this.numOperacoes++;
+
+
                 this.saldo -= valor;
                 return true;
             } else {
@@ -49,10 +114,10 @@ public class Conta {
     }
 
 
-    boolean transferir(Conta contaDestino, double valor) {
+    public boolean transferir(Conta contaDestino, double valor) {
         boolean saqueRealizado = this.sacar(valor);
 
-        if(saqueRealizado) {
+        if (saqueRealizado) {
             boolean depositoRealizado = contaDestino.depositar(valor);
             return depositoRealizado;
         } else {
@@ -60,20 +125,24 @@ public class Conta {
         }
     }
 
-    void imprimir() {
+
+    // Imprime os dados da conta
+    public void imprimir() {
         System.out.println("Dono da conta: " + this.dono.nome);
         System.out.println("CPF: " + this.dono.cpf);
-        System.out.println("Endereço: " + this.dono.endereco);
-        System.out.println("Idade: " + this.dono.idade);
         System.out.println("Gênero " + this.dono.sexo);
+        System.out.println("Idade: " + this.dono.idade);
+        System.out.println("Endereço: " + this.dono.endereco);
         System.out.println("Número da conta: " + this.numero);
         System.out.println("Saldo atual: " + this.saldo);
         System.out.println("Limite: " + this.limite);
     }
 
-    void extrato() {
-        for(int i = 0; i < numOperacoes; i++) {
-            System.out.println(operacoes[i].data+ "  " +operacoes[i].tipo+ "\t" +operacoes[i].valor);
+
+    // Imprime o histórico
+    public void extrato() {
+        for (int i = 0; i < numOperacoes; i++) {
+            System.out.println(operacoes[i].getData() + "  " + operacoes[i].getTipo() + "\t" + operacoes[i].getValor());
         }
     }
 }
